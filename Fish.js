67e06292e0b499d684af084bb6c7bba595e578ce
Anushka16July <87813443+Anushka16July@users.noreply.github.com>
2023-07-01 23:05:49 +0530

    Add files via upload

---
+++ Fish.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent("target-ring", {
+    init: function () {
+      for (var i = 1; i <= 20; i++) {
+        //id
+        var id = `ring${i}`;
+  
+        //position variables   
+        var posX =(Math.random() * 3000 + (-1000));      
+        var posY = (Math.random() * 2 + (-1));
+        var posZ = (Math.random() * 3000 + -1000);
+  
+        var position = { x: posX, y: posY, z: posZ };
+  
+       
+      }
+    } ,
+
+});
